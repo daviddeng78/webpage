35933b418de95aa22e86c38897287628cb6e3056
daviddeng78 <78229116+daviddeng78@users.noreply.github.com>
2021-06-01 16:06:26 -0400

    Update trivia.py

--- trivia.py
+++ trivia.py
@@ -1,7 +1,7 @@
 #Organizing the questions into a dictionary
 def createDictionary(filename):
-    openedFile = open(filename)
+    openedFile = open(filename, errors='ignore')
     readFile = openedFile.read()
     openedFile.close()
 
-    lines = readFile.split('\n')
+    strippedFile = readFile.strip()
