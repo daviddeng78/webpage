4dcc2f99bc891e06874655b2f41a235cc4526b13
daviddeng78 <78229116+daviddeng78@users.noreply.github.com>
2021-06-03 22:07:53 -0400

    Update trivia.py
    Function that sorts questions and answers into dictionary completed. Will be edited when we decide to create multiple categories.

--- trivia.py
+++ trivia.py
@@ -1,7 +1,25 @@
 #Organizing the questions into a dictionary
 def createDictionary(filename):
+
     openedFile = open(filename, errors='ignore')
     readFile = openedFile.read()
     openedFile.close()
 
-    strippedFile = readFile.strip()
+    qna = readFile.split('\n\n')
+
+    categoryDictionary = {}
+
+    for aqna in qna:
+        unstrippedQuestion = aqna[:aqna.index('\n')]
+        unstrippedAnswer = aqna[aqna.index('\n') + 1:]
+
+        strippedQuestion = unstrippedQuestion[unstrippedQuestion.index(':') + 2:]
+        strippedAnswer = unstrippedAnswer[unstrippedAnswer.index(':') + 2:]
+
+        categoryDictionary[strippedQuestion] = strippedAnswer
+
+    return categoryDictionary
+    
+
+
+    
